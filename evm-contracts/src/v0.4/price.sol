73bab184faf092147a7835534ff778f896a04d51
Wan, Michael Chien Min <65518554+Ptolemy-Techology@users.noreply.github.com>
2020-09-11 08:37:59 -0700

    Add files via upload

--- evm-contracts/src/v0.4/price.sol
+++ evm-contracts/src/v0.4/price.sol
@@ -1,1 +1,72 @@
+pragma solidity >=0.4.21 <0.7.0;
+import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.4/ChainlinkClient.sol";
+import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.4/vendor/Ownable.sol";
+contract price is ChainlinkClient, Ownable{
+    event HighestBidIncreased(address bidder, uint amount); // Event
+
+    function bid() public payable {
+        // ...
+        emit HighestBidIncreased(msg.sender, msg.value); // Triggering event
+    }
+    uint256 constant private ORACLE_PAYMENT = 1 * LINK;
+
+  uint256 public currentPrice;
+  int256 public changeDay;
+
+  event RequestFulfilled(
+    bytes32 indexed requestId,
+    uint256 indexed price
+  );
+
+ 
+
+  constructor() public Ownable() {
+    setPublicChainlinkToken();
+  }  
+
+  function requestPrice(address _oracle, string _jobId)
+  public
+  onlyOwner
+ 
+{
+  Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.fulfill.selector);
+   req.add("get", "http://localhost:3000");
+ // req.add("endpoint", "mwa-historic");
+ // req.add("coin", _coin);
+ // req.add("market", _market);
+  req.add("copyPath", "price_usd");
+  req.addInt("times", 100);
+  sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+} 
+    
+    
+   function fulfill(bytes32 _requestId, uint256 _price)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestFulfilled(_requestId, _price);
+    currentPrice = _price;
+  }
+  
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly { // solhint-disable-line no-inline-assembly
+      result := mload(add(source, 32))
+    }
+  } 
+    
+    
+}
+
+
+
+
+
+
+
+
 
